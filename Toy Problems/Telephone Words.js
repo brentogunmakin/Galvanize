f4891e110c8f0214ac9bae94cb01b2bd7c757200
brentogunmakin <brent.ogunmakin@us.af.mil>
2020-12-07 08:41:55 -0700

    idk

---
+++ Toy Problems/Telephone Words.js
@@ -0,0 +1,32 @@
+var phoneDigitsToLetters = {
+    0: '0',
+    1: '1',
+    2: 'ABC',
+    3: 'DEF',
+    4: 'GHI',
+    5: 'JKL',
+    6: 'MNO',
+    7: 'PQRS',
+    8: 'TUV',
+    9: 'WXYZ'
+  };
+
+
+  var telephoneWords = function (digitString) {
+    // TODO: return every combination that can be spelled on a phone with these digits
+    
+    let digitArray =  digitString.split('');
+    digitArray.forEach(digits => {
+        
+        return digits
+        
+    });
+
+    
+  };
+
+
+
+
+
+  console.log(telephoneWords('2745'));
